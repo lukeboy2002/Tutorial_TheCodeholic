d54a9e0b4002d87e6719a2ece708428335efa25f
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2024-10-28 20:23:07 +0100

    Create first page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,3 @@
+<h1>Hello welcome to my tutorial </h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab animi corporis culpa delectus dicta est eum, fuga harum
+    id ipsum minus obcaecati optio porro, quisquam quo sapiente sed tempore voluptatibus!</p>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    $person = [
+        'name' => 'John Doe',
+        'email' => 'john@doe.com',
+    ];
+    //    dump($person);
+    //    dd($person);
+    ddd($person);
+
     return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about');
 });
