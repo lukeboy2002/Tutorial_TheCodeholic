3f21c51d430f1842bfef38ce88c22e419a73ab64
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2024-10-30 20:11:37 +0100

    layout app.blade.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,12 +6,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view('home.index', [
-            'name' => 'Antoine',
-            'surname' => 'Hendriks',
-            'country' => 'nl',
-            'job' => '<b>Projectleader</b>',
-            'hobbies' => ['football', 'movies', 'webdesign'],
-        ]);
+        return view('home.index');
     }
 }
